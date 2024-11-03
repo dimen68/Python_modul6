e14580d4301d4d153d2c87c87000a48762dd208f
bochar68 <bochar.dg@gmail.com>
2024-11-03 16:31:45 +0300

    Применить сокрытие атрибутов и повторить наследование

---
+++ module_6_2.py
@@ -0,0 +1,53 @@
+#
+class Vehicle:
+    __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+
+    def __init__(self, owner, model, color, engine_power):
+        self.owner = owner
+        self.__model = model
+        self.__engine_power = engine_power
+        self.__color = color
+
+    def get_model(self):
+        return self.__model
+
+    def get_horsepower(self):
+        return self.__engine_power
+
+    def get_color(self):
+        return self.__color
+
+    def print_info(self):
+        print(f'Модель: {self.get_model()}')
+        print(f'Мощность двигателя: {self.get_horsepower()}')
+        print(f'Цвет: {self.get_color()}')
+        print(f'Владелец: {self.owner}')
+
+    def set_color(self, new_color):
+        if new_color.lower() in self.__COLOR_VARIANTS:
+            self.__color = new_color
+        else:
+            print(f'Нельзя сменить цвет на {new_color}')
+
+
+class Sedan(Vehicle):
+
+    def __init__(self, owner, model, color, engine_power, passenger_limit=5):
+        super().__init__(owner, model, color, engine_power)
+        self.__passenger_limit = passenger_limit
+
+
+if __name__ == '__main__':
+    # Текущие цвета __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+    vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
+
+    # Изначальные свойства
+    vehicle1.print_info()
+
+    # Меняем свойства (в т.ч. вызывая методы)
+    vehicle1.set_color('Pink')
+    vehicle1.set_color('BLACK')
+    vehicle1.owner = 'Vasyok'
+
+    # Проверяем что поменялось
+    vehicle1.print_info()
